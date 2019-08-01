6427823f92089db1e03a31bd6921f294026642ca
Matt <gr8bitmail@gmail.com>
2019-08-01 02:35:07 -0500

    teams

--- src/player.ts
+++ src/player.ts
@@ -1,12 +1,15 @@
 import { Card } from "./card";
+import { Team } from "./team";
 
 export class Player {
   dealer: boolean = false;
   cards: Array<Card>;
   nickname: string;
-  constructor(isDealer: boolean, nickname: string) {
+  team: Team;
+  constructor(isDealer: boolean, nickname: string, team: Team) {
     if (isDealer) this.dealer = isDealer;
     this.cards = [];
     this.nickname = nickname;
+    this.team = team;
   }
 }
