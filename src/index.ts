b8693a5f69d05f2b3c867b9ca77ac7feff263d5b
Matt <gr8bitmail@gmail.com>
2019-08-01 02:34:50 -0500

    teams

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,19 @@ import { Player } from "./player";
 import { GameState } from "./gamestate";
 import { Deck } from "./deck";
 import { Game } from "./game";
+import { Team } from "./team";
 
 const deck = new Deck();
 deck.shuffle();
 
+const teamOne = new Team("Team One");
+const teamTwo = new Team("Team Two");
+
 const players: Array<Player> = [
-  new Player(true, "joe"),
-  new Player(false, "matt"),
-  new Player(false, "kyle"),
-  new Player(false, "bob")
+  new Player(true, "joe", teamOne),
+  new Player(false, "matt", teamTwo),
+  new Player(false, "kyle", teamOne),
+  new Player(false, "bob", teamTwo)
 ];
 
 const game = new Game(deck, players);
